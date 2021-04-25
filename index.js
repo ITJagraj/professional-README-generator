6ef5b7f4e40de762ef0a85ae3b41da3e5c2f01f4
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-04-25 15:18:11 -0400

    userinput

--- index.js
+++ index.js
@@ -20,17 +20,50 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
+    // inquirer prompted questions for user//VIA command-line prompts
     inquirer.prompt([
-        type: "input",
-        name: "username",
-        messege: "Enter your Github username"
-    },
-    {
-        
-    }
-
-    ])
-}
+        {
+            // Takes user text input
+            type: "input",
+            name: "username",
+            message: "Enter Your Github Username"
+        },
+        {
+            // Takes user text input
+            type: "input",
+            name: "email",
+            message: "Enter Your Email Address"
+        },
+        {
+            // Takes user text input
+            type: "input",
+            name: "title",
+            message: "What's Your Projects Name?"
+        },
+        {
+            // Takes user text input
+            type: "input",
+            name: "description",
+            message: "Enter A Descrition For Your Project"
+        },
+        {
+            // Takes user text input
+            type: "input",
+            name: "installation",
+            message: "Enter Your Projects Installation Instructions"
+        },
+        {
+            // Takes user text input
+            type: "input",
+            name: "usage",
+            message: "Enter Usage Info"
+        },
+        {
+            // Takes user text input
+            type: "input",
+            name: "contribution",
+            message: "Enter Your Project Contribution Guidelines"
+        },
 
 // Function call to initialize app
 init();
