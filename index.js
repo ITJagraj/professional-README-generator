fd80b33c7a2640fd8813bcc195f4c7b884ceb6bd
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-04-25 15:08:08 -0400

    userinput

--- index.js
+++ index.js
@@ -8,59 +8,29 @@ const path = require("path");
 
 //creating a variable and requiring generateMarkdown.js
 var generateMarkdown = require(".util/generateMarkdown");
+const { type } = require('os');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: "input",
-        name: "title",
-        messege: "what is the title of the project",
-        validate: (nameInput) => {
-            if (nameInput) return true;
-            else {
-                console.log("please enter the title");
-                return false;
-            }
-        },
-    },
-    {
-        type: "input",
-        name: "id",
-        messege: "Please enter the employee's ID",
-        validate: (idInput) => {
-            if(idInput) {
-                return true;
-            }  
-        console.log("Please enter the employee's ID!");
-        return false;
-
-    }
-},
-{
-    type: "input",
-    name: "id",
-    messege: "Please enter the employee's ID",
-    validate: (idInput) => {
-        if(idInput) {
-            return true;
-        }  
-    console.log("Please enter the employee's ID!");
-    return false;  
-}
-];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     //writing file and syn it using path to join the working dir using the fileName
-    fs.writeFileSync(path.join(process.cwd(),fileName), data);
- }
+    fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt([
+        type: "input",
+        name: "username",
+        messege: "Enter your Github username"
+    },
+    {
         
+    }
+
     ])
- }
+}
 
 // Function call to initialize app
 init();
